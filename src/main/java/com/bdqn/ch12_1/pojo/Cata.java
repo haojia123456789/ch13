0ec9380a7b4f45bb0713651138c2f8709eb0ab8d
haojia <2021551911@qq.com>
2018-11-26 17:30:54 +0800

    11-8

--- src/main/java/com/bdqn/ch12_1/pojo/Cata.java
+++ src/main/java/com/bdqn/ch12_1/pojo/Cata.java
@@ -1,12 +1,14 @@
 package com.bdqn.ch12_1.pojo;
 
+import java.util.Map;
+
 public class Cata {
     private int id;
     private int level;
     private String name;
     private int parent;
 
-   private Map<String> maps;
+   private Map<String,String> maps;
 
 }
 
